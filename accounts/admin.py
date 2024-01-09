8fca254693f0687a09e7e9edd6f8100ad56b41d6
benup211 <benup211@gmail.com>
2024-01-09 20:47:10 +0545

    model creation

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(SchoolUser)
+class SchoolUserAdmin(admin.ModelAdmin):
+    list_display=['user','role']
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    list_display=['level','department']
+admin.site.register(Department)
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display=['student_user','department','grade']
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from common.models import *
 
-# Create your models here.
+class SchoolUser(CommonInfo):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    role=models.CharField(choices=r_choices,default="Student",max_length=50)
+    def __str__(self):
+        return self.user.username
+class Department(CommonInfo):
+    type=models.CharField(choices=dp_choices,max_length=100)
+    def __str__(self):
+        return self.type
+class Grade(CommonInfo):
+    level=models.CharField(choices=l_choices,max_length=10)
+    department=models.ForeignKey(Department,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.level
+class Student(CommonInfo):
+    student_user=models.ForeignKey(SchoolUser,on_delete=models.CASCADE)
+    email=models.EmailField()
+    address=models.CharField(max_length=100)
+    gender=models.CharField(choices=g_choices,max_length=20)
+    phone_no=models.CharField(max_length=15)
+    department=models.ForeignKey(Department,on_delete=models.CASCADE)
+    grade=models.ForeignKey(Grade,on_delete=models.CASCADE)
+    dob=models.DateField()
