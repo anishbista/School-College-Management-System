cce8f45df89784068e5e92fb79c24bc7ceb8e294
benup211 <benup211@gmail.com>
2024-01-10 19:45:19 +0545

    number 2:Announcement,Event and Holiday

---
+++ school_news/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from common.models import CommonInfo
+# Create your models here.
+class Holiday(CommonInfo):
+    name=models.CharField(max_length=50)
+    type=models.TextField(max_length=200)
+    start=models.DateField()
+    end=models.DateField()
+class Event(CommonInfo):
+    name=models.CharField(max_length=50)
+    descriptions=models.TextField()
+    start=models.DateTimeField()
+class Announcement(CommonInfo):
+    name=models.CharField(max_length=50)
+    descriptions=models.TextField()
