c1a809d25ee2df497547b1055864aeaa625a6047
anishbista92 <anishbista9235@gmail.com>
2024-01-11 09:21:38 +0545

    Templates,Static,Media Added

--- school_system/urls.py
+++ school_system/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from accounts.views import my_html_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("my-html/", my_html_view, name="my-html-view"),
 ]
