2aec3f840b1ba1db9945deb491b52297768f0435
anishbista92 <anishbista9235@gmail.com>
2024-01-18 22:25:04 +0545

    Add Assignment finished

--- teacher/models.py
+++ teacher/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from common.models import CommonInfo
+from accounts.models import Teacher, Student
+from django.core.validators import FileExtensionValidator
+
 
 # Create your models here.
+class Assignment(CommonInfo):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    start = models.DateField()
+    end = models.DateField()
+    image = models.ImageField(null=True, blank=True)
+    hw_file = models.FileField(
+        upload_to="assignment/",
+        validators=[FileExtensionValidator(["pdf", "docx"])],
+        null=True,
+        blank=True,
+    )
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- teacher/views.py
+++ teacher/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
-from django.views.generic import View
+from django.views.generic import View, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from accounts.models import *
+from .models import *
+from .forms import *
+from django.urls import reverse_lazy
 
 
 class TeacherDashboardView(LoginRequiredMixin, View):
@@ -15,3 +18,25 @@ class TeacherDashboardView(LoginRequiredMixin, View):
             messages.error(request, "You don't have permission to access this page")
             return redirect("login")
         return render(request, self.template_name)
+
+
+class AddAssignmentView(LoginRequiredMixin, CreateView):
+    model = Assignment
+    form_class = AssignmentForm
+    template_name = "teachers/assignment/add_assignment.html"
+    success_url = reverse_lazy("teacher_dashboard")
+
+    def form_valid(self, form):
+        print("Hello")
+        form.instance.teacher = self.request.user.teacher
+
+        # print(form.cleaned_data)
+        print("Form data:", form.data)
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        print("invalid")
+        print("Form data:", form.data)
+        print(form.errors)
+
+        return super().form_invalid(form)
