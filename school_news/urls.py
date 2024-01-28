1b572cc2f2556e89d002a75c1d170449259dbdab
benup211 <benup211@gmail.com>
2024-01-28 20:46:13 +0545

    student:news

--- school_news/urls.py
+++ school_news/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import AnnouncementView
-app_name='school_news'
+from .views import AnnouncementView,EventView,HolidayView
 
+app_name='school_news'
 urlpatterns=[
     path('announcement/',AnnouncementView.as_view(),name='announcement'),
+    path('event/',EventView.as_view(),name='event'),
+    path('holiday/',HolidayView.as_view(),name='holiday'),
 ]
--- school_news/views.py
+++ school_news/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
-from django.views import View
+from django.views.generic import View, ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import *
-class AnnouncementView(LoginRequiredMixin,View):
-    def get(self,request):
+class AnnouncementView(LoginRequiredMixin, View):
+    active_tab = "announcement"
+    template_name = "students/news/announcement.html"
+    def get(self,request ,*args, **kwargs):
         announcements=Announcement.objects.all()
-        return render(request,'school_news/announcement.html',{'announcements':announcements})
+        context={
+            'active_tab':self.active_tab,
+            'announcements':announcements
+        }
+        return render(request,self.template_name,context)
+class EventView(LoginRequiredMixin,View):
+    active_tab = "event"
+    template_name = "students/news/events.html"
+    def get(self,request ,*args, **kwargs):
+        events=Event.objects.all()
+        context={
+            'active_tab':self.active_tab,
+            'events':events
+        }
+        return render(request,self.template_name,context)
+class HolidayView(LoginRequiredMixin,View):
+    active_tab = "holiday"
+    template_name = "students/news/holiday.html"
+    def get(self,request ,*args, **kwargs):
+        holidays=Holiday.objects.all()
+        context={
+            'active_tab':self.active_tab,
+            'holidays':holidays
+        }
+        return render(request,self.template_name,context)
