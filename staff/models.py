4e59c10aa21f3a959af92afd677885222388bd7e
benup211 <benup211@gmail.com>
2024-02-07 20:43:18 +0545

    student and admin transportation with driver,bus,route,stop,schedule and alert

--- staff/models.py
+++ staff/models.py
@@ -24,3 +24,52 @@ class Borrowing(CommonInfo):
 def update_availability_status(sender, instance, **kwargs):
     instance.book.availability_status = True
     instance.book.save()
+
+class Driver(models.Model):
+    driver_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    contact_number = models.CharField(max_length=20)
+    def __str__(self) -> str:
+        return self.driver_name
+
+
+class Bus(models.Model):
+    bus_number = models.CharField(max_length=20)
+    capacity = models.IntegerField()
+    drive_by = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name_plural = "buses"
+    def __str__(self) -> str:
+        return self.bus_number
+
+class Route(models.Model):
+    route_name = models.CharField(max_length=100)
+    start_location = models.CharField(max_length=100)
+    end_location = models.CharField(max_length=100)
+    def __str__(self) -> str:
+        return self.route_name
+
+class Stop(models.Model):
+    stop_name = models.CharField(max_length=100)
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    stop_order = models.IntegerField()
+    def __str__(self) -> str:
+        return self.stop_name
+
+class Schedule(models.Model):
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    day_of_week = models.CharField(max_length=20)
+    departure_time = models.TimeField()
+    arrival_time = models.TimeField()
+
+class Alert(models.Model):
+    ALERT_TYPES = (
+        ('delay', 'Delay'),
+        ('schedule_change', 'Schedule Change'),
+    )
+
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    alert_type = models.CharField(max_length=20, choices=ALERT_TYPES)
+    alert_message = models.TextField()
+    alert_time = models.DateTimeField()
