5f0d63f085551de64b39a2b0143843b852081ccc
benup211 <benup211@gmail.com>
2024-02-08 19:53:33 +0545

    parent added:transportation and college_fee

--- parent/urls.py
+++ parent/urls.py
@@ -11,4 +11,12 @@ urlpatterns = [
     # path('courses/',CourseView.as_view(),name='courses'),
     # path('courses/<str:c_id>',CourseDetailView.as_view(),name='coursedetail'),
     path("gallery/", GalleryView.as_view(), name="gallery"),
+    path("tansporation/driver/",DriverView.as_view(),name="driver"),
+    path("tansporation/bus/",BusView.as_view(),name="bus"),
+    path("tansporation/route/",RouteView.as_view(),name="route"),
+    path("tansporation/stop/",StopView.as_view(),name="stop"),
+    path("tansporation/schedule/",ScheduleView.as_view(),name="schedule"),
+    path("tansporation/alert/",AlertView.as_view(),name="alert"),
+    path("college/fee/",FeeView.as_view(),name="feelist"),
+    path("college/payment/",PaymentView.as_view(),name="payment"),
 ]
--- parent/views.py
+++ parent/views.py
@@ -8,7 +8,7 @@ from gallery.models import *
 from .mixins import ParentRequiredMixin
 from teacher.models import Attendance
 from student.services import *
-
+from staff.models import *
 
 class ParentDashboardView(ParentRequiredMixin, View):
     template_name = "parents/dashboard.html"
@@ -90,3 +90,96 @@ class GalleryView(ParentRequiredMixin, View):
         gallerys = Gallery.objects.all()
         context = {"active_tab": self.active_tab, "gallerys": gallerys}
         return render(request, self.template_name, context)
+
+class DriverView(ParentRequiredMixin, View):
+    active_tab = "driver"
+    template_name = "parents/transportation/driver.html"
+
+    def get(self, request):
+        drivers = Driver.objects.all()
+        context = {"active_tab": self.active_tab, "drivers": drivers}
+        return render(request, self.template_name, context)
+
+
+class BusView(ParentRequiredMixin, View):
+    active_tab = "bus"
+    template_name = "parents/transportation/bus.html"
+
+    def get(self, request):
+        buses = Bus.objects.all()
+        context = {"active_tab": self.active_tab, "buses": buses}
+        return render(request, self.template_name, context)
+
+
+class RouteView(ParentRequiredMixin, View):
+    active_tab = "route"
+    template_name = "parents/transportation/route.html"
+
+    def get(self, request):
+        routes = Route.objects.all()
+        context = {"active_tab": self.active_tab, "routes": routes}
+        return render(request, self.template_name, context)
+
+
+class StopView(ParentRequiredMixin, View):
+    active_tab = "stop"
+    template_name = "parents/transportation/stop.html"
+
+    def get(self, request):
+        stops = Stop.objects.all()
+        context = {"active_tab": self.active_tab, "stops": stops}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        query = request.POST.get("stopquery")
+        stops = Stop.objects.filter(stop_name__icontains=query)
+        context = {"active_tab": self.active_tab, "stops": stops}
+        return render(request, self.template_name, context)
+
+
+class ScheduleView(ParentRequiredMixin, View):
+    active_tab = "schedule"
+    template_name = "parents/transportation/schedule.html"
+
+    def get(self, request):
+        schedules = Schedule.objects.all()
+        context = {"active_tab": self.active_tab, "schedules": schedules}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        query = request.POST.get("schedulequery")
+        schedules = Schedule.objects.filter(route__route_name__icontains=query)
+        context = {"active_tab": self.active_tab, "schedules": schedules}
+        return render(request, self.template_name, context)
+
+
+class AlertView(ParentRequiredMixin, View):
+    active_tab = "alert"
+    template_name = "parents/transportation/alert.html"
+
+    def get(self, request):
+        alerts = Alert.objects.all()
+        context = {"active_tab": self.active_tab, "alerts": alerts}
+        return render(request, self.template_name, context)
+
+#college fee
+class FeeView(ParentRequiredMixin,View):
+    active_tab = "fee"
+    template_name = "parents/college_fee/fee.html"
+    def get(self,request):
+        fees=Fee.objects.all()
+        context={
+            "active_tab": self.active_tab,
+            "fees":fees
+        }
+        return render(request,self.template_name,context)
+class PaymentView(ParentRequiredMixin,View):
+    active_tab = "payment"
+    template_name = "parents/college_fee/payment_list.html"
+    def get(self,request):
+        payments=Payment.objects.filter(student=request.user.parent.student)
+        context={
+            "active_tab": self.active_tab,
+            "payments":payments
+        }
+        return render(request,self.template_name,context)
